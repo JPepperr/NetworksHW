d6fa4b97122e6c783962d0fce9ac035ee9a831a1
JPepper <shidan3101@yandex.ru>
2022-12-12 00:42:59 +0300

    lab2 fix

--- lab2/main.py
+++ lab2/main.py
@@ -3,30 +3,27 @@ import subprocess
 
 
 HEADER_SIZE = 28
-MIN_MTU = 1
+MIN_MTU = 0
 MAX_MTU = 2000
 
 parser = argparse.ArgumentParser(description='Script finds MTU for given host')
 parser.add_argument('-c', default=1, type=int, action='store', help='Retry counts for one fixed size')
 parser.add_argument('host', type=str, action='store', help='Host')
 
+def make_ping(host, size, retry_count_str):
+    proc = subprocess.run(
+        ['ping', host, '-s', str(size), '-c', retry_count_str, '-M', 'do'],
+        capture_output=True
+    )
+    return proc.returncode, proc.stdout, proc.stderr
+
 def find_mtu(host: str, retry_count: int):
+    retry_count = str(retry_count)
     l = MIN_MTU
     r = MAX_MTU
     while r > l + 1:
         mid = (l + r) // 2
-
-        print(host, mid)
-
-        res = subprocess.run(
-            ['ping', host, '-s', str(mid), '-c', str(retry_count), '-M', 'do'],
-            stdin=subprocess.DEVNULL,
-            stdout=subprocess.DEVNULL,
-        )
-
-        print(res)
-
-        if res.returncode == 0:
+        if make_ping(host, mid, retry_count)[0] == 0:
             l = mid
         else:
             r = mid
@@ -36,7 +33,11 @@ def find_mtu(host: str, retry_count: int):
 
 def main():
     params = parser.parse_args()
-    print(find_mtu(params.host, params.c))
+    res = make_ping(params.host, 0, str(params.c))
+    if res[0] != 0:
+        print(f'Request failed with minimal size {HEADER_SIZE}.\nstdout:{res[1]}\nstderr:{res[2]}')
+    else:
+        print(find_mtu(params.host, params.c))
 
 if __name__=='__main__':
     main()
